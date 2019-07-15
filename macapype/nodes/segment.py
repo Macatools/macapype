062a8798b66d99a46eec5b77f942972630505e07
David Meunier <david_meunier_79@hotmail.fr>
2019-07-15 16:25:42 +0200

    modified wrap_antsAtroposN4 -> wrap_antsAtroposN4_dirty, with expriors as 3 files

--- macapype/nodes/segment.py
+++ macapype/nodes/segment.py
@@ -1,3 +1,49 @@
+def wrap_antsAtroposN4_dirty(dimension, brain_file, brainmask_file, numberOfClasses, ex_prior1, ex_prior2, ex_prior3):
+
+    import os
+    import shutil
+    from nipype.utils.filemanip import split_filename as split_f
+
+    _, prior_fname, prior_ext = split_f(ex_prior1) ## last element
+
+    ### copying file locally
+    dest = os.path.abspath("")
+
+    shutil.copy(ex_prior1,dest)
+    shutil.copy(ex_prior2,dest)
+    shutil.copy(ex_prior3,dest)
+
+    shutil.copy(brain_file,dest)
+    shutil.copy(brainmask_file,dest)
+
+    _, bmask_fname, bmask_ext = split_f(brainmask_file)
+    _, brain_fname, brain_ext = split_f(brain_file)
+
+    ### generating template_file
+    #TODO should be used by default
+    template_file = "tmp_%02d_allineate.nii.gz"
+
+    ### generating bash_line
+    os.chdir(dest)
+
+    out_pref = "segment_"
+    bash_line = "bash antsAtroposN4.sh -d {} -a {} -x {} -c {} -p {} -o {}".format(
+        dimension,brain_fname+ brain_ext ,bmask_fname+bmask_ext,numberOfClasses,template_file,out_pref)
+    print("bash_line : "+bash_line)
+
+    os.system(bash_line)
+
+    #out_files = [os.path.abspath(out_pref+"Segmentation"+fname+prior_ext) for fname in ["","Posteriors01","Posteriors02","Posteriors03"]]
+
+    seg_file = os.path.abspath(out_pref+"Segmentation"+prior_ext)
+    seg_post1_file = os.path.abspath(out_pref+"SegmentationPosteriors01"+prior_ext)
+    seg_post2_file = os.path.abspath(out_pref+"SegmentationPosteriors02"+prior_ext)
+    seg_post3_file = os.path.abspath(out_pref+"SegmentationPosteriors03"+prior_ext)
+
+    out_files = [seg_file,seg_post1_file,seg_post2_file,seg_post3_file]
+
+    #TODO surely more robust way can be used
+    return out_files, seg_file, seg_post1_file, seg_post2_file, seg_post3_file
 
 def wrap_antsAtroposN4(dimension, shft_aff_file, brainmask_file, numberOfClasses, ex_prior):
 
@@ -81,6 +127,7 @@ def wrap_NMT_subject_align(T1_file):
 ########### add Nwarp
 def add_Nwarp(list_prior_files):
 
+    import os
     from nipype.utils.filemanip import split_filename as split_f
 
     out_files = []
@@ -88,9 +135,9 @@ def add_Nwarp(list_prior_files):
 
         path, fname, ext = split_f(prior_file)
         #out_files.append(os.path.join(path,fname + "_Nwarp" + ext))
-        out_files.append(fname + "_Nwarp" + ext)
+        out_files.append(os.path.join(path,fname + "_Nwarp" + ext))
 
     for i in range(1,4):
-        out_files.append("tmp_%02d.nii.gz"%i)
+        out_files.append(os.path.join(path,"tmp_%02d.nii.gz"%i))
 
     return out_files
