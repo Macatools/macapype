4eec012bef915287422205aa357484b7fa77bf6e
David Meunier <david_meunier_79@hotmail.fr>
2019-07-15 16:26:00 +0200

    added get_elem

--- macapype/utils/misc.py
+++ macapype/utils/misc.py
@@ -17,3 +17,14 @@ def print_nii_data(nii_file):
     print (nii_file, data)
 
     return nii_file
+
+
+def get_elem(list_elem, index_elem):
+    assert isinstance(list_elem,list),'Error, list_elem should be a list'
+    assert 0 <= index_elem and index_elem <= len(list_elem),\
+        ('error with index {}, does not math a list with {} elements'.format(index_elem, len(list_elem)))
+
+    elem  = list_elem[index_elem]
+    print(elem)
+
+    return elem
