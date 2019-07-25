f22236e214db6a03332bc646fdc564ebe57aca11
David Meunier <david_meunier_79@hotmail.fr>
2019-07-25 11:23:24 +0200

    modify with datasource from previous_analysis

--- examples/full_segment_pnh_kepkee.py
+++ examples/full_segment_pnh_kepkee.py
@@ -9,14 +9,7 @@ fsl.FSLCommand.set_default_output_type('NIFTI_GZ')
 
 import os
 
-from macapype.pipelines.preproc import create_average_align_pipe
-from macapype.pipelines.denoise import (
-    create_denoised_cropped_pipe, create_cropped_denoised_pipe)
-
-from macapype.pipelines.correct_bias import create_correct_bias_pipe
-from macapype.pipelines.extract_brain import create_brain_extraction_pipe
-from macapype.pipelines.segment import (create_brain_segment_pipe,
-    create_full_segment_pipe)
+from macapype.pipelines.segment import create_full_segment_pipe
 
 from macapype.utils.misc import show_files
 
@@ -26,7 +19,8 @@ main_path = "/hpc/meca/users/loh.k/test_pipeline"
 site = "sbri"
 subject_ids = ['032311']
 
-prev_pipe_path = os.path.join(main_path, "test_pipeline_kepkee_by_kepkee","segment_pnh_subpipes")
+prev_pipe_path = os.path.join(main_path, "test_pipeline_kepkee_by_kepkee",
+                              "segment_pnh_subpipes")
 
 def create_infosource():
     infosource = pe.Node(interface=niu.IdentityInterface(fields=['subject_id']),name="infosource")
@@ -35,17 +29,18 @@ def create_infosource():
     return infosource
 
 def create_datasource():
-   datasource = pe.Node(interface=nio.DataGrabber(infields=['subject_id'],outfields=['T1','T2']),name = 'datasource')
+   datasource = pe.Node(interface=nio.DataGrabber(infields=['subject_id'],outfields=['T1','T2', 'mask']),name = 'datasource')
    datasource.inputs.base_directory = prev_pipe_path
-   datasource.inputs.template = '/%s/%s/sub-%s_ses-001_run-*_%s.nii.gz'
+   datasource.inputs.template = '%s/%s%s/%s%s/%ssub-%s_ses-001_run-*_%s.nii.gz'
    datasource.inputs.template_args = dict(
-       T1=[["average_align_pipe","av_T1",
-            #"sub-",'subject_id',"ses-001","anat",'subject_id',"T1w"]],
-       T2=[["average_align_pipe","align_T2_on_T1",
-            #"sub-",'subject_id',"ses-001","anat",'subject_id',"T2w"]],
-       mask=[["brain_extraction_pipe","smooth_mask"
-             # site,'subject_id',"mask",'subject_id',
-             "_nice!"]],
+       T1=[["average_align_pipe","_subject_id_", 'subject_id',"","av_T1","",
+            'subject_id',"T1w_flirt"]],
+       T2=[["average_align_pipe","_subject_id_", 'subject_id',"","align_T2_on_T1",
+            "",'subject_id',"T2w_flirt"]],
+       mask=[["brain_extraction_pipe", "_subject_id_",
+              'subject_id', "","smooth_mask", "avg_", 'subject_id',
+              #'T1w_maths_aonlm_denoised_roi_brain_bin_bin_nice']],
+              'T1w_maths_aonlm_denoised_roi_brain_bin_bin']],
        )
    datasource.inputs.sort_filelist = True
 
@@ -84,23 +79,17 @@ def create_main_workflow():
     ############################################## Preprocessing ################################
     ##### segment_pnh
 
-    print('segment_pnh')
+    print('full_segment_pnh')
 
-    #segment_pnh = create_segment_pnh_subpipes()
-    segment_pnh =
     ################### full_segment
     brain_segment_pipe = create_full_segment_pipe(crop_list = [(88, 144), (14, 180), (27, 103)], sigma = 2)
 
-    seg_pipe.connect(datasource, "T1",brain_segment_pipe,'inputnode.T1')
-    seg_pipe.connect(datasource, "T2", brain_segment_pipe,'inputnode.T2')
+    main_workflow.connect(datasource, "T1",
+                          brain_segment_pipe, 'inputnode.preproc_T1')
+    main_workflow.connect(datasource, "T2",
+                          brain_segment_pipe, 'inputnode.preproc_T2')
 
-    seg_pipe.connect(brain_extraction_pipe,"smooth_mask.out_file",brain_segment_pipe,"inputnode.brain_mask")
-
-
-
-
-    main_workflow.connect(datasource,'T1',segment_pnh,'inputnode.T1')
-    main_workflow.connect(datasource,'T2',segment_pnh,'inputnode.T2')
+    main_workflow.connect(datasource, "mask",brain_segment_pipe,"inputnode.brain_mask")
 
     return main_workflow
 
