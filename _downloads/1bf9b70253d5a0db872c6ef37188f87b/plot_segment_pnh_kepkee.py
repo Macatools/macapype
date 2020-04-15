a649a0fff60869d9946911bd076baa2419497e19
David Meunier <david_meunier_79@hotmail.fr>
2020-04-15 17:23:19 +0200

    Make install

--- _downloads/1bf9b70253d5a0db872c6ef37188f87b/plot_segment_pnh_kepkee.py
+++ _downloads/1bf9b70253d5a0db872c6ef37188f87b/plot_segment_pnh_kepkee.py
@@ -13,19 +13,42 @@ Plot the results of a segmentation
 import os
 import os.path as op
 
+import json
+import pprint
+
 import nipype.pipeline.engine as pe
 
 from nipype.interfaces.utility import IdentityInterface
 import nipype.interfaces.io as nio
 
-###############################################################################
-#Running workflow
-#==================
+################################################################################
+##Running workflow
+##==================
 
-#from macapype.utils.utils_tests import load_test_data
+#from macapype.utils.utils_tests import load_test_data, format_template
 #from macapype.pipelines.full_segment import create_full_segment_pnh_subpipes
 
-#my_path = "/hpc/crise/meunier.d/"
+
+#from macapype.utils.utils_tests import load_test_data
+
+#package_directory = os.path.dirname(os.path.abspath(__file__))
+#params_file = '{}/../workflows/params_segment_pnh_kepkee.json'.format(package_directory)
+#params = json.load(open(params_file))
+
+#print(params)
+#pprint.pprint(params)
+
+#if "general" in params.keys() and "my_path" in params["general"].keys():
+    #my_path = params["general"]["my_path"]
+#else:
+    ##my_path = "/home/INT/meunier.d/Data/"
+    #my_path = "/hpc/crise/meunier.d/"
+    ##my_path = "/hpc/neopto/USERS/racicot/data/"
+
+#nmt_dir = load_test_data('NMT_v1.2', path_to = my_path)
+#params_template = format_template(nmt_dir, 'NMT_v1.2')
+#print (params_template)
+
 
 #data_path = load_test_data("data_test_macapype", path_to = my_path)
 
@@ -33,18 +56,15 @@ import nipype.interfaces.io as nio
 #T1_file = op.join(data_path, "sub-Apache_ses-01_T1w.nii")
 #T2_file = op.join(data_path, "sub-Apache_ses-01_T2w.nii")
 
-#from macapype.utils.utils_tests import load_test_data
-
-#nmt_dir = load_test_data('NMT_v1.2', path_to = my_path)
-#atlasbrex_dir = load_test_data('AtlasBREX', path_to = my_path)
-
 ## running workflow
-#segment_pnh = create_full_segment_pnh_subpipes(nmt_dir, atlasbrex_dir)
+#segment_pnh = create_full_segment_pnh_subpipes(params=params,
+                                               #params_template=params_template,
+                                               #segment=False,
+                                               #name = "segment_pnh_subpipes_ziggy")
 #segment_pnh.base_dir = my_path
 
 #segment_pnh.inputs.inputnode.T1 = T1_file
 #segment_pnh.inputs.inputnode.T2 = T2_file
-
 
 #segment_pnh.write_graph(graph2use="colored")
 #segment_pnh.run()
