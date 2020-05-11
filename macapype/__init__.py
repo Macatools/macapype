d1bd94395732a3e3eb546890a1be4b7ce8bc1c87
David Meunier <david_meunier_79@hotmail.fr>
2020-05-11 15:07:41 +0200

    automated version in _version.py -> setup.py, doc/conf.py and macapype.__version__ in python/ipython

--- macapype/__init__.py
+++ macapype/__init__.py
@@ -1,3 +1,10 @@
 from . import pipelines  # noqa
 from . import nodes  # noqa
 from . import utils  # noqa
+
+__version__ = "unknown"
+try:
+    from ._version import __version__ # noqa
+except ImportError:
+    # We're running in a tree that doesn't have a _version.py
+    pass
--- setup.py
+++ setup.py
@@ -4,9 +4,26 @@
 from setuptools import setup, find_packages
 import os
 
+import re
+
+verstr = "unknown"
+try:
+    verstrline = open('macapype/_version.py', "rt").read()
+except EnvironmentError:
+    pass # Okay, there is no version file.
+else:
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    mo = re.search(VSRE, verstrline, re.M)
+    if mo:
+        verstr = mo.group(1)
+    else:
+        raise RuntimeError("unable to find version in yourpackage/_version.py")
+
+
+
 setup(
     name="macapype",
-    version='0.1.1',
+    version=verstr,
     packages=find_packages(),
     author="David Meunier, Bastien Cagna",
     description="Pipeline for anatomic processing for macaque",
