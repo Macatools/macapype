493a2f94e7253906f3efd9d69dec13b3a6a3fcc5
David Meunier <david_meunier_79@hotmail.fr>
2020-05-26 09:46:15 +0200

    hasattr

--- macapype/utils/utils_nodes.py
+++ macapype/utils/utils_nodes.py
@@ -73,11 +73,9 @@ class BIDSDataGrabberParams(BIDSDataGrabber):
 
 def output_exists(node, output_name):
     print (node.outputs)
-    #def_outputs = list(node.outputs.keys())
-    def_outputs = list(node.outputs.get_traitsfree().keys())
-    print (def_outputs)
-    if output_name in def_outputs:
 
-        print("found {} in {}".format(output_name, node))
+    if hasattr(node.outputs,output_name):
+
+        print("found {}".format(output_name))
 
     0/0
