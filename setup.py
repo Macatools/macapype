86fc3757eba6d85a250a2b7b5bbb26d937ffefa0
bc264254 <!2EelagFuo>
2020-12-09 18:48:32 +0100

    Update requires

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
-import os
-
 import re
-
-from distutils.core import setup, Extension
+from setuptools import find_packages
+from distutils.core import setup
 
 
 verstr = "unknown"
@@ -21,7 +18,6 @@ else:
         verstr = mo.group(1)
     else:
         raise RuntimeError("unable to find version in yourpackage/_version.py")
-
 
 
 try:
@@ -50,5 +46,6 @@ except ModuleNotFoundError as e:
         author="David Meunier, Bastien Cagna",
         description="Pipeline for anatomic processing for macaque",
         license='BSD 3',
-        install_requires=["nipype", "networkx>=2.0", "pybids", "skimage"])
+        install_requires=[
+            "nipype", "nilearn", "networkx>=2.0", "pybids", "scikit-image", "slam@git+https://github.com/gauzias/slam"])
 
