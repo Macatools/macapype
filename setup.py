8fe7c1244e736fec0b1664268aeb1888ba508bf1
bc264254 <!2EelagFuo>
2020-12-15 15:38:41 +0100

    try using specific branch for slam

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ try:
         description="Pipeline for anatomic processing for macaque",
         license='BSD 3',
         install_requires=[
-            "nipype", "nilearn", "networkx>=2.0", "pybids", "scikit-image", "slam@git+https://github.com/BastienCagna/slam"],
+            "nipype", "nilearn", "networkx>=2.0", "pybids", "scikit-image", "slam@git+https://github.com/gauzias/slam.git@change-setup-metadata"],
         distclass=distutils.command.bdist_conda.CondaDistribution,
         conda_buildnum=1)
 
@@ -47,5 +47,5 @@ except ModuleNotFoundError as e:
         description="Pipeline for anatomic processing for macaque",
         license='BSD 3',
         install_requires=[
-            "nipype", "nilearn", "networkx>=2.0", "pybids", "scikit-image", "slam@git+https://github.com/BastienCagna/slam.git"])
+            "nipype", "nilearn", "networkx>=2.0", "pybids", "scikit-image", "slam@git+https://github.com/gauzias/slam.git@change-setup-metadata"])
 
