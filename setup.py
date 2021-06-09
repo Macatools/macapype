8ee7576d96219d5f4cf7b91cf51fc9ea05fc9d73
David Meunier <david_meunier_79@hotmail.fr>
2021-06-09 16:23:36 +0200

    added manifes and json files for pip install

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import re
-from setuptools import find_packages
-from distutils.core import setup
+from setuptools import find_packages, setup
 
 
 required_packages=[
@@ -23,33 +22,16 @@ else:
     else:
         raise RuntimeError("unable to find version in yourpackage/_version.py")
 
-try:
-    import distutils.command.bdist_conda
+print("Will not build conda module")
 
-    setup(
-        name="macapype",
-        version=verstr,
-        packages=find_packages(),
-        author="macatools team",
-        description="Pipeline for anatomic processing for macaque",
-        license='BSD 3',
-        install_requires=required_packages,
-        entry_points = {
-            'console_scripts': ['segment_pnh = workflows.segment_pnh:main']},
-        distclass=distutils.command.bdist_conda.CondaDistribution,
-        conda_buildnum=1)
-
-except ModuleNotFoundError as e:
-
-    print("Will not build conda module")
-
-    setup(
-        name="macapype",
-        version=verstr,
-        packages=find_packages(),
-        author="macatools team",
-        description="Pipeline for anatomic processing for macaque",
-        license='BSD 3',
-        entry_points = {
-            'console_scripts': ['segment_pnh = workflows.segment_pnh:main']},
-        install_requires= required_packages)
+setup(
+    name="macapype",
+    version=verstr,
+    packages=find_packages(),
+    author="macatools team",
+    description="Pipeline for anatomic processing for macaque",
+    license='BSD 3',
+    entry_points = {
+        'console_scripts': ['segment_pnh = workflows.segment_pnh:main']},
+    install_requires= required_packages,
+    include_package_data=True)
