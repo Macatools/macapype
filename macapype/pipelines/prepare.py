e1ad8d1b2a7f0478b216d376f157332854eaa120
David Meunier <david_meunier_79@hotmail.fr>
2022-07-29 13:39:47 +0200

    apply_crop_aladin_T2, "out_file",

--- macapype/pipelines/prepare.py
+++ macapype/pipelines/prepare.py
@@ -503,7 +503,7 @@ def create_short_preparation_pipe(params, params_template={},
             data_preparation_pipe.connect(crop_z_T1, "out_roi",
                                           denoise_T1, 'input_image')
 
-            data_preparation_pipe.connect(crop_z_T1, "out_roi",
+            data_preparation_pipe.connect(apply_crop_aladin_T2, "out_file",
                                           denoise_T2, 'input_image')
 
         # outputs
@@ -529,7 +529,7 @@ def create_short_preparation_pipe(params, params_template={},
         else:
             data_preparation_pipe.connect(crop_z_T1, "out_roi",
                                           outputnode, 'preproc_T1')
-            data_preparation_pipe.connect(crop_z_T2, "out_roi",
+            data_preparation_pipe.connect(apply_crop_aladin_T2, "out_file",
                                           outputnode, 'preproc_T2')
     return data_preparation_pipe
 
