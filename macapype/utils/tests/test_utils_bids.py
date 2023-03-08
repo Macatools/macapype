2ec949df3b1187585890398119c04042db15c3c1
David Meunier <david_meunier_79@hotmail.fr>
2023-03-08 14:08:26 +0100

    print(datasink)

--- macapype/utils/tests/test_utils_bids.py
+++ macapype/utils/tests/test_utils_bids.py
@@ -5,11 +5,13 @@ from macapype.utils.utils_bids import create_datasink
 def test_create_datasink_sub_ses():
     iterables = [('subject', "mysub"), ('session', 'myses')]
     datasink = create_datasink(iterables)
+    print(datasink)
 
     assert True
 
 def test_create_datasink_sub():
     iterables = [('subject', "mysub")]
     datasink = create_datasink(iterables)
+    print(datasink)
 
     assert True
