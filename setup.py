14f49ae46dbbdf9d9f9ca9f4453381958a993ccf
David Meunier <david_meunier_79@hotmail.fr>
2024-04-10 16:41:26 +0200

    relaxing nibabel

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 
 required_packages = [
     "nipype", "nilearn", "networkx",
-    "pybids", "scikit-image", "nibabel==3.2.2",
+    "pybids", "scikit-image", "nibabel",
     "numpy==1.23", "brain-slam"]
 
 verstr = "unknown"
