11386d8abea0688afbf3d6607960aa5672bd7a23
David Meunier <david_meunier_79@hotmail.fr>
2024-05-23 11:34:19 +0200

    use_T2 in brain_segment_pipe

--- macapype/pipelines/full_pipelines.py
+++ macapype/pipelines/full_pipelines.py
@@ -1445,39 +1445,41 @@ def create_full_ants_subpipes(
     seg_pipe.connect(inputnode, 'indiv_params',
                      brain_segment_pipe, 'inputnode.indiv_params')
 
-    if "masked_correct_bias_pipe" in params.keys():
-        seg_pipe.connect(
-            masked_correct_bias_pipe, 'outputnode.mask_debiased_T1',
-            brain_segment_pipe, 'inputnode.masked_debiased_T1')
+    if 'use_T2' in params['brain_segment_pipe'].keys():
 
-        if "correct_bias_pipe" in params.keys():
+        if "masked_correct_bias_pipe" in params.keys():
             seg_pipe.connect(
-                correct_bias_pipe, "outputnode.debiased_T1",
-                brain_segment_pipe, 'inputnode.debiased_T1')
+                masked_correct_bias_pipe, 'outputnode.mask_debiased_T2',
+                brain_segment_pipe, 'inputnode.masked_debiased_T1')
 
-        elif "N4debias" in params.keys():
+            if "correct_bias_pipe" in params.keys():
+                seg_pipe.connect(
+                    correct_bias_pipe, "outputnode.debiased_T2",
+                    brain_segment_pipe, 'inputnode.debiased_T1')
+
+            elif "N4debias" in params.keys():
+                seg_pipe.connect(
+                    N4debias_T2, "output_image",
+                    brain_segment_pipe, 'inputnode.debiased_T1')
+
+            elif "fast" in params.keys():
+                seg_pipe.connect(
+                    fast_T2, ("restored_image", show_files),
+                    brain_segment_pipe, 'inputnode.debiased_T1')
+
+            else:
+                seg_pipe.connect(
+                    data_preparation_pipe, 'outputnode.preproc_T2',
+                    brain_segment_pipe, 'inputnode.debiased_T1')
+
+        elif "debias" in params.keys():
             seg_pipe.connect(
-                N4debias_T1, "output_image",
-                brain_segment_pipe, 'inputnode.debiased_T1')
+                debias, 't2_debiased_brain_file',
+                brain_segment_pipe, 'inputnode.masked_debiased_T1')
 
-        elif "fast" in params.keys():
             seg_pipe.connect(
-                fast_T1, ("restored_image", show_files),
+                debias, 't2_debiased_file',
                 brain_segment_pipe, 'inputnode.debiased_T1')
-
-        else:
-            seg_pipe.connect(
-                data_preparation_pipe, 'outputnode.preproc_T1',
-                brain_segment_pipe, 'inputnode.debiased_T1')
-
-    elif "debias" in params.keys():
-        seg_pipe.connect(
-            debias, 't1_debiased_brain_file',
-            brain_segment_pipe, 'inputnode.masked_debiased_T1')
-
-        seg_pipe.connect(
-            debias, 't1_debiased_file',
-            brain_segment_pipe, 'inputnode.debiased_T1')
 
     else:
 
@@ -1504,6 +1506,68 @@ def create_full_ants_subpipes(
         seg_pipe.connect(
             restore_mask_T1, 'out_file',
             brain_segment_pipe, 'inputnode.masked_debiased_T1')
+
+    else:
+
+        if "masked_correct_bias_pipe" in params.keys():
+            seg_pipe.connect(
+                masked_correct_bias_pipe, 'outputnode.mask_debiased_T1',
+                brain_segment_pipe, 'inputnode.masked_debiased_T1')
+
+            if "correct_bias_pipe" in params.keys():
+                seg_pipe.connect(
+                    correct_bias_pipe, "outputnode.debiased_T1",
+                    brain_segment_pipe, 'inputnode.debiased_T1')
+
+            elif "N4debias" in params.keys():
+                seg_pipe.connect(
+                    N4debias_T1, "output_image",
+                    brain_segment_pipe, 'inputnode.debiased_T1')
+
+            elif "fast" in params.keys():
+                seg_pipe.connect(
+                    fast_T1, ("restored_image", show_files),
+                    brain_segment_pipe, 'inputnode.debiased_T1')
+
+            else:
+                seg_pipe.connect(
+                    data_preparation_pipe, 'outputnode.preproc_T1',
+                    brain_segment_pipe, 'inputnode.debiased_T1')
+
+        elif "debias" in params.keys():
+            seg_pipe.connect(
+                debias, 't1_debiased_brain_file',
+                brain_segment_pipe, 'inputnode.masked_debiased_T1')
+
+            seg_pipe.connect(
+                debias, 't1_debiased_file',
+                brain_segment_pipe, 'inputnode.debiased_T1')
+
+        else:
+
+            if "correct_bias_pipe" in params.keys():
+                seg_pipe.connect(
+                    correct_bias_pipe, "outputnode.debiased_T1",
+                    brain_segment_pipe, 'inputnode.debiased_T1')
+
+            elif "N4debias" in params.keys():
+                seg_pipe.connect(
+                    N4debias_T1, "output_image",
+                    brain_segment_pipe, 'inputnode.debiased_T1')
+
+            elif "fast" in params.keys():
+                seg_pipe.connect(
+                    fast_T1, ("restored_image", show_files),
+                    brain_segment_pipe, 'inputnode.debiased_T1')
+
+            else:
+                seg_pipe.connect(
+                    data_preparation_pipe, 'outputnode.preproc_T1',
+                    brain_segment_pipe, 'inputnode.debiased_T1')
+
+            seg_pipe.connect(
+                restore_mask_T1, 'out_file',
+                brain_segment_pipe, 'inputnode.masked_debiased_T1')
 
     # outputnode
     seg_pipe.connect(brain_segment_pipe, 'outputnode.segmented_file',
