546e6faed468feb02b38ce9a16f40d74f419146c
David Meunier <david_meunier_79@hotmail.fr>
2024-12-09 15:46:37 +0100

    removed versioneer

--- setup.py
+++ setup.py
@@ -6,7 +6,26 @@
 """
 import sys
 from setuptools import setup
-import versioneer
+
+import re
+
+
+def _get_version():
+
+    verstr = "unknown"
+    try:
+        verstrline = open('macapype/_version.py', "rt").read()
+    except EnvironmentError:
+        pass  # Okay, there is no version file.
+    else:
+        VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+        mo = re.search(VSRE, verstrline, re.M)
+        if mo:
+            verstr = mo.group(1)
+        else:
+            raise RuntimeError("unable to find version in yourpackage/_version.py")
+    return verstr
+
 
 # Give setuptools a hint to complain if it's too old a version
 # 30.3.0 allows us to put most metadata in setup.cfg
@@ -18,7 +37,6 @@ SETUP_REQUIRES += ["wheel"] if "bdist_wheel" in sys.argv else []
 if __name__ == "__main__":
     setup(
         name="macapype",
-        version=versioneer.get_version(),
-        cmdclass=versioneer.get_cmdclass(),
+        version=_get_version(),
         setup_requires=SETUP_REQUIRES,
     )
