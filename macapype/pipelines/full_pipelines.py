61760d24c83f74cb3a8edd2a0544701cf2691a47
David Meunier <david_meunier_79@hotmail.fr>
2025-03-04 11:08:24 +0100

    OK native_to_stereo_trans was not exported if crop_t1!!!!!!

--- macapype/pipelines/full_pipelines.py
+++ macapype/pipelines/full_pipelines.py
@@ -1828,16 +1828,13 @@ def create_full_T1_ants_subpipes(params_template, params_template_stereo,
     seg_pipe.connect(data_preparation_pipe, "outputnode.stereo_padded_T1",
                      outputnode, "stereo_padded_T1")
 
-    if "short_preparation_pipe" in params.keys():
-        if "crop_T1" not in params["short_preparation_pipe"].keys():
+    seg_pipe.connect(data_preparation_pipe,
+                        "outputnode.native_to_stereo_trans",
+                        outputnode, 'native_to_stereo_trans')
 
-            seg_pipe.connect(data_preparation_pipe,
-                             "outputnode.native_to_stereo_trans",
-                             outputnode, 'native_to_stereo_trans')
-
-            seg_pipe.connect(data_preparation_pipe,
-                             "outputnode.stereo_to_native_trans",
-                             outputnode, 'stereo_to_native_trans')
+    seg_pipe.connect(data_preparation_pipe,
+                        "outputnode.stereo_to_native_trans",
+                        outputnode, 'stereo_to_native_trans')
 
     # ######### correct_bias
     if "N4debias" in params.keys():
