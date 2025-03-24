7547956981fd0aa8c23cb4666d801eb6660f4fcc
David Meunier <david_meunier_79@hotmail.fr>
2025-03-24 15:44:31 +0100

    removed data_macaque_sphinx and all previous docs

--- macapype/utils/tests/test_utils_tests.py
+++ macapype/utils/tests/test_utils_tests.py
@@ -16,7 +16,7 @@ def test_server_amubox():
     tmp_path = make_tmp_dir()
     name = op.join(tmp_path, "data_test_macaque.zip")
 
-    code = "RDxdxzmX89xcABG"
+    code = "BdpeSWACJbrNp8r"
     server = "https://amubox.univ-amu.fr"
     add = "{}/public.php?service=files&t={}&download".format(server, code)
     cmd = "wget  --no-check-certificate  \"{}\" -O {}".format(add, name)
@@ -44,19 +44,6 @@ def test_data_test_macaque():
 
     T1_file = op.join(data_path, "sub-Apache_ses-01_T1w.nii")
     T2_file = op.join(data_path, "sub-Apache_ses-01_T2w.nii")
-
-    params = {"t1_file": T1_file, "t2_file": T2_file, "aT2": True}
-
-    bet_crop = NodeParams(interface=T1xT2BET(), params=params,  # noqa
-                          name="bet_crop")
-
-
-def test_data_test_sphinx_macaque():
-
-    data_path = load_test_data("data_test_sphinx_macaque")
-
-    T1_file = op.join(data_path, "sub-ziggy_T1w.nii")
-    T2_file = op.join(data_path, "sub-ziggy_T2w.nii")
 
     params = {"t1_file": T1_file, "t2_file": T2_file, "aT2": True}
 
